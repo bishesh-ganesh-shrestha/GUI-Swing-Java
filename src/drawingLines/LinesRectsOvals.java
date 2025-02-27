7403e6a819c6440cd38f586d1aa75df14882cb3c
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-02-27 13:48:24 +0545

    quickfix(Lines in JFrame)

--- src/drawingLines/LinesRectsOvals.java
+++ src/drawingLines/LinesRectsOvals.java
@@ -1,12 +1,16 @@
 package drawingLines;
 
+import java.awt.Color;
 import javax.swing.JFrame;
 
 public class LinesRectsOvals {
     public static void main(String[] args) {
-         JFrame frame = new JFrame("Drawing Lines");
-         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         frame.setSize(400, 180);
-         frame.setVisible(true);
+        JFrame frame = new JFrame("Drawing Lines");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        LinesRectsOvalsJPanel lro = new LinesRectsOvalsJPanel();
+        lro.setBackground(Color.WHITE);
+        frame.add(lro);
+        frame.setSize(400, 180);
+        frame.setVisible(true);
     }
 }
